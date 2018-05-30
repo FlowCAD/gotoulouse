1f0e27e94e397b13d04449bcf4b1a2cd4ee74fd1
FlowCAD <florian.cadoz@gmail.com>
2018-05-30 23:20:50 +0200

    OSM copyright attribution added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -14,7 +14,9 @@ export class MapComponent implements OnInit {
   ngOnInit() {
     let northEastBound = L.latLng(43.68, 1.68), southWestBound = L.latLng(43.52, 1.35), bounds = L.latLngBounds(northEastBound, southWestBound);
     const mymap = L.map('map').setView([43.599560, 1.441079], 13).setMaxBounds(bounds);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
   }
 
 }
