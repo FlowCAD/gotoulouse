b944a6d6a8e2aa6202272683fba82161e105e3e3
FlowCAD <florian.cadoz@gmail.com>
2018-06-26 23:04:26 +0200

    Issue with icon in markers fixed and binding popup added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,29 +15,23 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     let northEastBound = L.latLng(43.75, 1.76), southWestBound = L.latLng(43.43, 1.02), bounds = L.latLngBounds(northEastBound, southWestBound);
-    const mymap = L.map('map', {minZoom: 12}).setView([43.6, 1.44], 13).setMaxBounds(bounds);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    const mymap = L.map('map', { minZoom: 12 }).setView([43.6, 1.44], 13).setMaxBounds(bounds);
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
 
-    
-  const myIcon = L.icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
-  });
-  L.marker([43.6, 1.44], {icon: myIcon}).addTo(mymap);
-  
-  const style_mapicon = L.AwesomeMarkers.icon({
-    prefix: 'fa',
-    icon: 'star',
-    markerColor: 'red'
-  });
+    const subwayMarkerSymbol = L.AwesomeMarkers.icon({ icon: 'fa-subway', prefix: 'fa', markerColor: 'blue', iconColor: 'white' });
 
-  this.http.get('https://data.toulouse-metropole.fr/api/v2/catalog/datasets/stations-de-metro/records?rows=100&pretty=true&timezone=UTC').subscribe((data: any) => {
-    data.records.forEach((mydata:any) => {
-      L.marker([mydata.record.fields.geo_shape.geometry.coordinates[1], mydata.record.fields.geo_shape.geometry.coordinates[0]], {icon: style_mapicon}).addTo(mymap);
-      console.log(mydata.record.fields);
+    const redMarker = L.AwesomeMarkers.icon({ icon: 'fa-car', markerColor: 'red', iconColor: 'white', prefix: 'fa' });
+    L.marker([43.61, 1.45], { icon: redMarker }).addTo(mymap);
+
+    this.http.get('https://data.toulouse-metropole.fr/api/v2/catalog/datasets/stations-de-metro/records?rows=100&pretty=true&timezone=UTC').subscribe((data: any) => {
+      data.records.forEach((mydata: any) => {
+        L.marker([mydata.record.fields.geo_shape.geometry.coordinates[1], mydata.record.fields.geo_shape.geometry.coordinates[0]], { icon: subwayMarkerSymbol })
+          .addTo(mymap)
+          .bindPopup(mydata.record.fields.nom + " (ligne " + mydata.record.fields.ligne + ")");
+      });
     });
-  });
 
   }
 }
