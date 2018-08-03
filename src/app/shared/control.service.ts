99bf0fbd2bad8964d57fcfe123661dd94c091cdf
FlowCAD <florian.cadoz@gmail.com>
2018-08-03 23:26:31 +0200

    Typescript fixes

--- src/app/shared/control.service.ts
+++ src/app/shared/control.service.ts
@@ -6,35 +6,42 @@ import * as L from 'leaflet';
   providedIn: 'root'
 })
 export class ControlService {
+  // tslint:disable-next-line:max-line-length
   private mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>';
+  // tslint:disable-next-line:max-line-length
   private mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=XXXXXXXX';
   private osmAttr = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-  private osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'; 
+  private osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  private osmHotUrl = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
 
-  public get OSM() : any {
+  public get OSM(): any {
     return L.tileLayer(this.osmUrl, { attribution: this.osmAttr});
   }
-  
-  public get MBGrey() : any {
+
+  public get OSMHot(): any {
+    return L.tileLayer(this.osmHotUrl, { attribution: this.osmAttr});
+  }
+
+  public get MBGrey(): any {
     return L.tileLayer(this.mbUrl, { id: 'mapbox.light', attribution: this.mbAttr });
   }
 
-  public get MBSat() : any {
+  public get MBSat(): any {
     return L.tileLayer(this.mbUrl, { id: 'mapbox.satellite', attribution: this.mbAttr });
   }
-    
-  public get MBStreets() : any {
+
+  public get MBStreets(): any {
     return L.tileLayer(this.mbUrl, { id: 'mapbox.streets', attribution: this.mbAttr });
   }
-    
+
   public getBaseLayers() {
     const baseMaps = {
-      "OpenStreetMap": this.OSM,
-      "Plan gris": this.MBGrey,
-      "Plan": this.MBStreets,
-      "Satellite": this.MBSat
+      'OpenStreetMap': this.OSM,
+      'OSM Hot': this.OSMHot,
+      'Plan gris': this.MBGrey,
+      'Plan': this.MBStreets,
+      'Satellite': this.MBSat
     };
-
     return baseMaps;
   }
 }
--- src/app/shared/opendata.service.ts
+++ src/app/shared/opendata.service.ts
@@ -11,10 +11,12 @@ export class OpendataService {
   ) { }
 
   public getSubways() {
-    return this.http.get('https://data.toulouse-metropole.fr/api/v2/catalog/datasets/stations-de-metro/records?rows=100&pretty=true&timezone=UTC')
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://data.toulouse-metropole.fr/api/v2/catalog/datasets/stations-de-metro/records?rows=100&pretty=true&timezone=UTC');
   }
 
   public getBikes() {
-    return this.http.get('https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=XXXXXXXX')
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=XXXXXXXX');
   }
 }
