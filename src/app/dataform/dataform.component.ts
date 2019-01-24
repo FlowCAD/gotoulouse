5be600068e3fb42e5359348c2a28ce967609f8c2
Florian CADOZ <florian.cadoz@gmail.com>
2019-01-24 14:12:34 +0100

    Frontend : Link to backend for Places

--- src/app/dataform/dataform.component.ts
+++ src/app/dataform/dataform.component.ts
@@ -5,7 +5,7 @@ import { Subscription } from 'rxjs';
 
 import { DataService } from '@app/shared/services/data.service';
 
-import { EnumGenres, EnumSousGenres, Genre } from '@app/shared/interface';
+import { Place, EnumGenres, EnumSousGenres, Genre } from '@app/shared/interface';
 
 @Component({
   selector: 'app-dataform',
@@ -18,8 +18,10 @@ export class DataFormComponent implements OnInit {
   sousGenre = new FormControl();
 
   public genres: Genre[];
-  public genreSubscription: Subscription;
+  public genresSubscription: Subscription;
   public sousGenres: any[] = [];
+  public places: Place[];
+  public placesSubscription: Subscription;
 
   @Input() lat: string;
   @Input() lng: string;
@@ -31,7 +33,8 @@ export class DataFormComponent implements OnInit {
 
   ngOnInit() {
     this.dataService.getGenresFromServer();
-    this.genreSubscription = this.dataService.genresSubject.subscribe(
+    this.dataService.getPlacesFormServer();
+    this.genresSubscription = this.dataService.genresSubject.subscribe(
       (genres: Genre[]) => {
         this.genres = genres;
       }
@@ -39,30 +42,36 @@ export class DataFormComponent implements OnInit {
     this.dataService.emitGenres();
 
     // TODO Load sousgenre thanks to genres
-    Object.keys(EnumSousGenres).forEach(key => {
-      this.sousGenres.push({
-        value: key,
-        viewValue: EnumSousGenres[key]
-      });
-    });
+    // Object.keys(EnumSousGenres).forEach(key => {
+    //   this.sousGenres.push({
+    //     value: key,
+    //     viewValue: EnumSousGenres[key]
+    //   });
+    // });
 
     this.dataForm = this.formBuilder.group({
       titleFormControl: ['', [Validators.required]],
       latFormControl: [''],
       lngFormControl: [''],
-      descriptionFormControl: ['']
+      descriptionFormControl: [''],
+      genreControl: ['']/*,
+      sousGenreControl: ['']*/
     });
   }
 
-  onValidate(value: Boolean): void {
-    console.log('validation');
-    if (value === true) {
-      console.log('Titre: ', this.dataForm.get('titleFormControl').value);
-      console.log('Latitude: ', this.lat);
-      console.log('Longitude: ', this.lng);
-      console.log('Description: ', this.dataForm.get('descriptionFormControl').value);
-      console.log('Genre: ', this.selectedGenre);
-      console.log('Sous-genre: ', this.sousGenre.value);
-    }
+  onValidate(): void {
+    const newPlace: Place = {
+      nom: this.dataForm.get('titleFormControl').value,
+      latitude: this.dataForm.get('latFormControl').value,
+      longitude: this.dataForm.get('lngFormControl').value,
+      description: this.dataForm.get('descriptionFormControl').value,
+      genre: this.dataForm.get('genreControl').value.name,
+      sous_genre: null,
+      // sous_genre: this.dataForm.get('sousGenreControl').value,
+      date_creation: new Date(),
+      creator: 'Admin'
+    };
+    console.log('newPlace: ', newPlace);
+    this.dataService.addNewPlace(newPlace);
   }
 }
