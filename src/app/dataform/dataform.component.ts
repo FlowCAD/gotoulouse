b8b1a72b10c9f89be691f55efaaff240fae75667
FlowCAD <florian.cadoz@gmail.com>
2019-01-25 23:20:16 +0100

    Frontend : Sous-Genre controler working now

--- src/app/dataform/dataform.component.ts
+++ src/app/dataform/dataform.component.ts
@@ -6,7 +6,7 @@ import { Subscription } from 'rxjs';
 
 import { DataService } from '@app/shared/services/data.service';
 
-import { Place, EnumGenres, EnumSousGenres, Genre } from '@app/shared/interface';
+import { Place, Genre } from '@app/shared/interface';
 
 @Component({
   selector: 'app-dataform',
@@ -24,23 +24,14 @@ export class DataFormComponent implements OnInit, OnDestroy {
   public places: Place[];
   public placesSubscription: Subscription;
 
-  @Input() lat: string;
-  @Input() lng: string;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private snackBar: MatSnackBar,
-    private dataService: DataService
-  ) { }
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.getGenresFromServer();
     this.dataService.getPlacesFormServer();
-    this.genresSubscription = this.dataService.genresSubject.subscribe(
-      (genres: Genre[]) => {
-        this.genres = genres;
-      }
-    );
+    this.genresSubscription = this.dataService.genresSubject.subscribe((genres: Genre[]) => {
+      this.genres = genres;
+    });
     this.dataService.emitGenres();
     this.initForm();
   }
@@ -55,8 +46,8 @@ export class DataFormComponent implements OnInit, OnDestroy {
       latFormControl: ['', [Validators.required]],
       lngFormControl: ['', [Validators.required]],
       descriptionFormControl: [''],
-      genreControl: ['', [Validators.required]]/*,
-      sousGenreControl: ['']*/
+      genreFormControl: ['', [Validators.required]],
+      sousGenreFormControl: [[]]
     });
   }
 
@@ -66,10 +57,9 @@ export class DataFormComponent implements OnInit, OnDestroy {
       latitude: this.dataForm.get('latFormControl').value,
       longitude: this.dataForm.get('lngFormControl').value,
       description: this.dataForm.get('descriptionFormControl').value,
-      genre: this.dataForm.get('genreControl').value.name,
-      sous_genre: null,
-      // sous_genre: this.dataForm.get('sousGenreControl').value,
-      date_creation: new Date(),
+      genre: this.dataForm.get('genreFormControl').value,
+      sous_genre: this.dataForm.get('sousGenreFormControl').value,
+      date_creation: new Date().toUTCString(),
       creator: 'Admin'
     };
     console.log('newPlace: ', newPlace);
