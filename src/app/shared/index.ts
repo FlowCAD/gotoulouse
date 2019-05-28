004e9d637788c5c639fb61fa736f93c623989dd7
Florian CADOZ <florian.cadoz@gmail.com>
2019-05-28 17:48:58 +0200

    settings : recreated GH-Pages Branch

--- src/app/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from '@app/shared/shared.module';
-export * from '@app/shared/loader/loader.component';
--- src/app/shared/marker.model.ts
+++
@@ -1,3 +0,0 @@
-export class MarkerSymbol {
-  constructor(public icon: string, public prefix: string, public markerColor: string, public iconColor: string) {}
-}
